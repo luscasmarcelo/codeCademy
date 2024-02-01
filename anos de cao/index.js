0aed340a233b15eaa4c509cb71e276c1dd4b8523
Lucas Marcelo <lucas.sampaio4@gmail.com>
2024-02-01 14:46:15 -0300

    manipulando operadores e variaveis em JS

---
+++ anos de cao/index.js
@@ -0,0 +1,14 @@
+//declarando a variável idade humana
+let myAge = 28 
+//declarando os dois primeiros anos do cachorro
+let earlyYears = 2
+earlyYears *= 10.5
+//definindo nova variavel
+let laterYears = myAge - 2
+//calculando o numero de anos do cão contabilizados por seus ultimos anos
+laterYears *= 4
+
+myAgeInDogYears = earlyYears + laterYears
+let myName = "LUCAS".toLowerCase()
+
+console.log(`My name is ${myName}. i am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
