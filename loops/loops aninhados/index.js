4c82e5fd7f331c2ecca1f35823a8b87379f222c7
Lucas Marcelo <lucas.sampaio4@gmail.com>
2024-02-27 14:36:40 -0300

    conceito de loop aninhado

---
+++ loops/loops aninhados/index.js
@@ -0,0 +1,10 @@
+const bobsFollowers = ["caio", "bruno", "Payet", "lucas"];
+const tinasFollowers = ["Payet", "caio", "julia"];
+let mutualFollowers = []
+for (let i = 0; i < bobsFollowers.length; i++) {
+  for (let j = 0; j < tinasFollowers.length; j++) {
+    if (bobsFollowers[i] === tinasFollowers[j]) {
+      mutualFollowers.push(bobsFollowers[i])
+    }
+  }
+}
