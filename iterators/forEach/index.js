86af758e51be0e45eb1666b405e9dbf2f1b314a8
Lucas Marcelo <lucas.sampaio4@gmail.com>
2024-03-05 13:25:29 -0300

    introdução a iteradores

---
+++ iterators/forEach/index.js
@@ -0,0 +1,6 @@
+const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+//executa o msm código para cada elemnto da matriz utilizando o método forEach()
+fruits.forEach(function(fruitsItem) {
+  console.log("I want to eat a " + fruitsItem)
+})
