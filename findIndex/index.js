4fec6a221c3a538c5d6a0540eb7d47d814f2f034
Lucas Marcelo <lucas.sampaio4@gmail.com>
2024-03-07 13:48:17 -0300

    conceito do método findIndex()

---
+++ findIndex/index.js
@@ -0,0 +1,10 @@
+const animals = ['hippo', 'tiger', 'lion', 'seal', 'cheetah', 'monkey', 'salamander', 'elephant'];
+
+const foundAnimal = animals.findIndex(posicao => {
+  return posicao === 'elephant'
+});
+console.log(foundAnimal)
+const startsWithS = animals.findIndex(animal => {
+  return animal[0] === 's' ? true : false;
+});
+console.log(startsWithS)
