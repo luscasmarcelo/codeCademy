efddca12e724c9f56db223bad088f33cd17896d6
Lucas Marcelo <lucas.sampaio4@gmail.com>
2024-03-11 20:30:14 -0300

    atualização

---
+++ objetos/introdução objetos/index.js
@@ -0,0 +1,13 @@
+let spaceship = {
+    'Fuel Type' : 'Turbo Fuel',
+    'Active Mission' : true,
+    homePlanet : 'Earth', 
+    numCrew: 5
+   };
+  
+  let propName =  'Active Mission';
+  
+  /*criando variável e atribuindo ao spaceship a prop 'Active Mission*/
+  let isActive = spaceship['Active Mission'];
+  /*chamando no consoleo valor da variavel 'active mission' do objeto spaceship*/
+  console.log(spaceship['Active Mission'])
